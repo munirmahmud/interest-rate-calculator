c40a6f07aee3e3349dcab8f9ced9e2fbfa1b1989
munirmahmud <munirmahmud6@gmail.com>
2024-06-24 20:04:17 +0600

    Project done

---
+++ script.js
@@ -0,0 +1,20 @@
+let calculateBtn = document.getElementById("calculate-btn");
+let result = document.getElementById("result");
+
+let calculate = () => {
+  let principal = Number(document.getElementById("principal").value);
+  let rate = Number(document.getElementById("rate").value);
+  let years = Number(document.getElementById("years").value);
+
+  let simpleInterest = (principal * rate * years) / 100;
+
+  let amount = principal + simpleInterest;
+
+  document.getElementById("principalAmount").innerHTML = principal.toFixed(2);
+  document.getElementById("totalInterest").innerHTML =
+    simpleInterest.toFixed(2);
+  document.getElementById("totalAmount").innerHTML = amount.toFixed(2);
+};
+
+calculateBtn.addEventListener("click", calculate);
+window.addEventListener("load", calculate);
